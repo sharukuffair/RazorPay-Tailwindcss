656951e91ec6cbba431aab856835d4837601becb
sharukuffair <sharukuffair1998@gmail.com>
2023-02-07 08:22:18 +0530

    featuesection2 progress
    building feature section2

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,9 @@ module.exports = {
         deepBlueHead: "#162f56",
         gray2: "#525a76",
       },
+      backgroundImage: {
+        "feature2-img": "url('assets/feature-section-2BG.svg')",
+      },
     },
   },
   plugins: [],
